4609bffe4f5fbedd2746fe3ea6aedbe6cd96e4c7
Alexis Mourier <alexis.mourier@grenoble-inp.org>
2024-11-29 11:18:26 +0100

    Fix : Agents are now only created on empty cells during the first generation

--- scripts/model.py
+++ scripts/model.py
@@ -1,3 +1,5 @@
+import random
+
 from mesa import Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
@@ -19,13 +21,16 @@ class CrowdModel(Model):
         self.schedule = RandomActivation(self)
         self.exit = exit_pos  # Position(s) of the exit(s)
 
-        # Create agents
+        # Create agents only on empty cells
+        empty_cells = [(x, y) for x in range(self.grid.width) for y in range(self.grid.height) if self.grid.is_cell_empty((x, y))]
         for i in range(n_agents):
+            if len(empty_cells) == 0:
+                break
             agent = PedestrianAgent(i, self)
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(agent, (x, y))
+            cell_i = random.randint(0, len(empty_cells)-1)
+            self.grid.place_agent(agent, empty_cells[cell_i])
             self.schedule.add(agent)
+            empty_cells.pop(cell_i)
 
     def step(self):
         self.schedule.step()
@@ -33,3 +38,5 @@ class CrowdModel(Model):
 
 if __name__ == "__main__":
     model = CrowdModel(0, 4, 4, [(2,2), (2,3)], (1,3))
+    for _ in range(100):
+        model.step()
